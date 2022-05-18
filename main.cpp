417aba991851f38b4a236dfbec81e4c59b7bd9d1
Michael Bond <delayner2018@gmail.com>
2022-05-18 14:39:06 +0300

    Object lifetime(Stack/Scope lifetimes)

--- main.cpp
+++ main.cpp
@@ -2,47 +2,37 @@
 #include <iostream>
 #include <string>
 
-struct Vector2{
-float x, y;
+class Entity{
+public:
+    Entity(){
+        std::cout << "Create entity!" << std::endl;
+    }
 
-    Vector2(float x, float y)
-        : x(x), y(y) {}
-
-        Vector2 Add(const Vector2& other) const
-        {
-            return Vector2(x + other.x, y + other.y);
-        }
-
-        Vector2 operator+(const Vector2& other) const
-        {
-            return Add(other);
-        }
-
-        Vector2 Multiply(const Vector2& other) const
-        {
-            return Vector2(x * other.x, y * other.y);
-        }
-
-        Vector2 operator*(const Vector2& other) const
-        {
-            return Multiply(other);
-        }
+    ~Entity(){
+        std::cout << "Delete entity!" << std::endl;
+    }
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vector2& other) {
-    stream << other.x << ", " << other.y;
-    return stream;
-}
+class ScopedPtr{
+private:
+    Entity* m_Ptr;
+public:
+    ScopedPtr(Entity* ptr)
+        : m_Ptr(ptr)
+    {
+    }
+
+    ~ScopedPtr()
+    {
+        delete m_Ptr;
+    }
+};
 
 int main(){
-    Vector2 position(4.0f, 4.0f);
-    Vector2 speed(0.5f, 1.5f);
-    Vector2 powerup(1.1f, 1.1f);
 
-    Vector2 result1 = position.Add(speed.Multiply(powerup));
-    Vector2 result2 = position + speed * powerup;
-
-    std::cout << result2 <<std::endl;
-
+    {
+        ScopedPtr e = new Entity();//allocated on a stack
+        //Entity* en = new Entity(); - without delete automatically
+    }
     std::cin.get();
 }
