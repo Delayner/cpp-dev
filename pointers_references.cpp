27e0c90f4fc7b13205705f2d0fc9e6c0638c0da5
Michael Bond <delayner2018@gmail.com>
2022-05-22 12:52:06 +0300

    The Arrow Operator v2

--- pointers_references.cpp
+++ pointers_references.cpp
@@ -8,17 +8,31 @@ public:
     void Print() const { std::cout << "Hello!" << std::endl; }
 };
 
+class ScopedPtr
+{
+private:
+    Entity* m_Obj;
+public:
+    ScopedPtr(Entity* entity)
+        : m_Obj(entity)
+    {
+    }
 
+    ~ScopedPtr()
+    {
+        delete m_Obj;
+    }
+
+    Entity* operator->()
+    {
+        return m_Obj;
+    }
+};
 
 int main()
 {
-    Entity e;
-    e.Print();
-
-    Entity* ptr = &e;
-    // Entity& entity = *ptr;
-    // (*ptr).Print();
-    ptr->Print();
+    ScopedPtr entity = new Entity();
+    entity->Print();
 
     std::cin.get();
 }
